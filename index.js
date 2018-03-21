de937b56c28b3dc310b57ab4bf9a69b1bd6fd155
barestu <fheartnet@gmail.com>
2018-03-21 21:06:29 +0700

    Promise Multiple File

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function readFilePromise() {
+function readFilePromise(file_name) {
   // psst, the promise should be around here...
+  return new Promise((resolve, reject) => {
+    fs.readFile(file_name, 'utf8', (err, data) => {
+      sleep.sleep(2)
+      if (!err) {
+        data = JSON.parse(data);
+        resolve(data);
+      } else {
+        reject(`(!) Error: Wrong file type/path!`);
+      }
+    })
+  })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  readFilePromise(parentFileName)
+  .then(dataParent => {
+    return readFilePromise(childrenFileName)
+    .then(dataChildren => {
+      for (let i in dataParent) {
+        let arrChildren = [];
+        for (let j in dataChildren) {
+          if (dataParent[i].last_name == dataChildren[j].family) {
+            arrChildren.push(dataChildren[j].full_name);
+          }
+        }
+        dataParent[i].childrens = arrChildren;
+      }
+      console.log(dataParent);
+    })
+  })
+  .catch(error => {
+    console.log(error);
+  })
 }
 
-matchParentsWithChildrens('./parents.json', './childrens.json');
+// matchParentsWithChildrens('./parents.json', './childrens.json');
 console.log("Notification : Data sedang diproses !");
 
 // for Release 2
